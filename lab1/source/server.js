b40764001d902b314da8fdfc073d2f5c863db987
WPapuga <wojtek.papuga@yahoo.com>
2023-03-13 14:59:36 +0100

    dodanie obsługi dwóch plików

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -38,5 +38,43 @@ app.get('/:file', (req, res) => {
     }           
 })
 
+app.get('/:file1/:file2', (req, res) => {
+    const file1Ext = path.extname(req.params.file1);
+    const file2Ext = path.extname(req.params.file2);
+
+    if(file1Ext == '.mp4'){
+        res.send(`<body>
+                    <h1>Hello World Player!</h1>
+                    <video controls>
+                        <source src="${req.params.file1}" type="video/mp4">
+                        przeglądarka nie obsługuje video
+                    </video>
+                    <audio controls>
+                        <source src="${req.params.file2}" type="audio/mpeg">
+                        przeglądarka nie obsługuje audio
+                    </audio>
+                 </body>`);
+    }
+    else if(file1Ext == '.mp3'){
+        res.send(`<body>
+                    <h1>Hello World Player!</h1>
+                    <video controls>
+                        <source src="${req.params.file2}" type="video/mp4">
+                        przeglądarka nie obsługuje video
+                    </video>
+                    <audio controls>
+                        <source src="${req.params.file1}" type="audio/mpeg">
+                        przeglądarka nie obsługuje audio
+                    </audio>
+                  </body>`);       
+                }
+    else{
+        res.send(`<body>
+                    <h1>Hello World Player!</h1>
+                    <h2>Podales zle pliki</h2>
+                  </body>`);
+    }           
+})
+
 
 app.listen(4080)

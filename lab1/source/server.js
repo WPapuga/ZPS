f1e1138865048775dd362704755174f8758d3b3a
WPapuga <wojtek.papuga@yahoo.com>
2023-03-13 14:44:46 +0100

    zachowanie strony przy nie podaniu żadnego pliku

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`<body>
+                <h1>Hello World Player!</h1>
+                <h2>Nie podałeś żadnego pliku w urlu</h2>
+              </body>`);
 })
+
 
 app.listen(4080)

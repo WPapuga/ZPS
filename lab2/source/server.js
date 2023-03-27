7dd0061332b3ec237b43c30605148da034d2fbad
WPapuga <wojtek.papuga@yahoo.com>
2023-03-27 14:31:17 +0200

    poprawienie poprzedniego zadania

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -5,82 +5,26 @@ const app = express();
 app.use(express.static('publicFiles'));
 
 app.get('/', (req, res) => {
-    res.send(`<body>
-                <h1>Hello World Player!</h1>
-                <h2>Nie podałeś żadnego pliku w urlu</h2>
-              </body>`);
-})
-
-app.get('/:file', (req, res) => {
-    const fileExt = path.extname(req.params.file);
-    const filePath = '/' + req.params.file;
-    if(fileExt == '.mp4'){
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <video id="videoPlayer" controls>
-                        <source src="${filePath}" type="video/mp4">
+    var page = `<body>
+                    <h1>Hello World Player!</h1>`;
+    if(req.query.videoFile != undefined){
+        if(path.extname(req.query.videoFile) == '.mp4'){
+            page += `<video id="videoPlayer" controls>
+                        <source src="${req.query.videoFile}" type="video/mp4">
                         przeglądarka nie obsługuje video
-                    </video>
-                 </body>`);
+                    </video>`
+        }
     }
-    else if(fileExt == '.mp3'){
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <audio id="audioPlayer" controls>
-                        <source src="${filePath}" type="audio/mpeg">
+    if(req.query.audioFile != undefined){
+        if(path.extname(req.query.audioFile) == '.mp3'){
+            page += `<audio id="audioPlayer" controls>
+                        <source src="${req.query.audioFile}" type="audio/mpeg">
                         przeglądarka nie obsługuje audio
-                    </audio>
-                  </body>`);       
-                }
-    else{
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <h2>Podales zle pliki</h2>
-                  </body>`);
-    }           
-})
-
-app.get('/:file1/:file2', (req, res) => {
-    const file1Ext = path.extname(req.params.file1);
-    const file2Ext = path.extname(req.params.file2);
-    var videoFile;
-    var audioFile;
-    if(file1Ext == '.mp4'){
-        videoFile = '/' + req.params.file1;
-        audioFile = '/' + req.params.file2;
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <video id="videoPlayer" controls>
-                        <source src="${videoFile}" type="video/mp4">
-                        przeglądarka nie obsługuje video
-                    </video>
-                    <audio id="audioPlayer" controls>
-                        <source src="${audioFile}" type="audio/mpeg">
-                        przeglądarka nie obsługuje audio
-                    </audio>
-                 </body>`);
+                    </audio>`;
+        }
     }
-    else if(file1Ext == '.mp3'){
-        videoFile = '/' + req.params.file2;
-        audioFile = '/' + req.params.file1;
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <video id="videoPlayer" controls>
-                        <source src="${videoFile}" type="video/mp4">
-                        przeglądarka nie obsługuje video
-                    </video>
-                    <audio id="audioPlayer" controls>
-                        <source src="${audioFile}" type="audio/mpeg">
-                        przeglądarka nie obsługuje audio
-                    </audio>
-                  </body>`);       
-                }
-    else{
-        res.send(`<body>
-                    <h1>Hello World Player!</h1>
-                    <h2>Podales zle pliki</h2>
-                  </body>`);
-    }       
+    var page = `</body>`;
+    res.send(page);
 })
 
 
